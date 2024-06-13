131b9b0bf97fc2676dd3ee98bbdc89ddcc380694
Nathan Coates <30511439+imajacket@users.noreply.github.com>
2024-06-13 16:22:31 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/components/*.ts",
+    "./**.html",
+      "./pages/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  safelist: [
+      "input-error",
+  ],
+  plugins: [],
+}
